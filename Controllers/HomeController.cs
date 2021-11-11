7d8fdccedbf8cbd3d9d84d45b95205533f36ad86
Jon Ahmeti <jon.ahmeti@ks.asseco-see.local>
2021-11-11 13:36:02 +0100

    Uploading initial MVC project files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Furlough.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Furlough.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
