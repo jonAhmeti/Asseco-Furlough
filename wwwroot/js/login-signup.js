fd7269780c90d44650edcd9c02c819769937aaef
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2021-11-15 14:47:03 +0100

    Partial created

--- wwwroot/js/login-signup.js
+++ wwwroot/js/login-signup.js
@@ -1,41 +1,29 @@
-// var loginHTMLform =
-//   '<div class="form-floating mb-3">' +
-//   '<input type="text" class="form-control" name="username" placeholder="Username">' +
-//   '<label for="username">Username</label>' +
-//   "</div>" +
-//   '<div class="form-floating mb-2">' +
-//   '<input type="password" class="form-control" name="password" placeholder="Password">' +
-//   '<label for="password">Password</label>' +
-//   "</div>" +
-//   '<div class="d-grid gap-2 mb-3">' +
-//   '<button class="btn btn-lg btn-outline-primary" type="submit">' +
-//   '<i class="fa fa-sign-in"></i> Login</button>' +
-//   "</div>" +
-//   '<a href="#" class="float-end">Forgot Password?</a>' +
-//   '<div class="form-check mb-3">' +
-//   '<input type="checkbox" class="form-check-input" name="remember-me">' +
-//   '<label class="form-check-label" for="remember-me">Remember Me</label>' +
-//   "</div>";
-// var signupHTMLform =
-//   '<div class="form-floating mb-3">' +
-//   '<input type="text" class="form-control" name="fullname" placeholder="Fullname">' +
-//   '<label for="fullname">Fullname</label>' +
-//   "</div>";
-// $(document).ready(function () {
-//   $("#login").addClass("active");
-//   $("#form").html(loginHTMLform);
-// });
-// $("#login").on("click", function () {
-//   if ($("#signup").hasClass("active")) {
-//     $("#signup").removeClass("active");
-//     $("#login").addClass("active");
-//     $("#form").html(loginHTMLform);
-//   }
-// });
-// $("#signup").on("click", function () {
-//   if ($("#login").hasClass("active")) {
-//     $("#login").removeClass("active");
-//     $("#signup").addClass("active");
-//     $("#form").html(signupHTMLform);
-//   }
-// });
+$(document).ready(function () {
+  let login = $("#login");
+  let signup = $("#signup");
+  let formWrapper = $("#formWrapper");
+  $(login).on("click", function () {
+    $(signup).attr("active", "false");
+    $(login).attr("active", "true");
+
+    $.ajax({
+        type: "GET",
+        url:"/Home/Partial/Login.cshtml",
+        success: function(result) {
+            $(formWrapper).html(result);
+        }
+    });
+  });
+  $(signup).on("click", function () {
+    $(login).attr("active", "false");
+    $(signup).attr("active", "true");
+
+    $.ajax({
+        type: "GET",
+        url:"/Home/Partial/Signup.cshtml",
+        success: function(result) {
+            $(formWrapper).html(result);
+        }
+    });
+  });
+});
