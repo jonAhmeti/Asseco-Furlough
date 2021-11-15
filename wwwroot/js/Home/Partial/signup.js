829ef42ab3506af323207c8a17b53247ad70e121
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2021-11-15 16:09:30 +0100

    Sign up - slide buttons added & created css and js for signup.cshtml

---
+++ wwwroot/js/Home/Partial/signup.js
@@ -0,0 +1,6 @@
+$( function () {
+    let employeeSection = $("#employeeSection0");
+    let userSection = $("#userSection");
+    let rightarrow = $("#arrowright");
+    let leftarrow = $("#arrowleft");
+});
