6bdf70bf431d0dc3c54b9109fd5233a13975a22b
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2021-11-15 14:11:06 +0100

    Log in form in jQuery, Sign up form created halfway

---
+++ wwwroot/js/login-signup.js
@@ -0,0 +1,41 @@
+// var loginHTMLform =
+//   '<div class="form-floating mb-3">' +
+//   '<input type="text" class="form-control" name="username" placeholder="Username">' +
+//   '<label for="username">Username</label>' +
+//   "</div>" +
+//   '<div class="form-floating mb-2">' +
+//   '<input type="password" class="form-control" name="password" placeholder="Password">' +
+//   '<label for="password">Password</label>' +
+//   "</div>" +
+//   '<div class="d-grid gap-2 mb-3">' +
+//   '<button class="btn btn-lg btn-outline-primary" type="submit">' +
+//   '<i class="fa fa-sign-in"></i> Login</button>' +
+//   "</div>" +
+//   '<a href="#" class="float-end">Forgot Password?</a>' +
+//   '<div class="form-check mb-3">' +
+//   '<input type="checkbox" class="form-check-input" name="remember-me">' +
+//   '<label class="form-check-label" for="remember-me">Remember Me</label>' +
+//   "</div>";
+// var signupHTMLform =
+//   '<div class="form-floating mb-3">' +
+//   '<input type="text" class="form-control" name="fullname" placeholder="Fullname">' +
+//   '<label for="fullname">Fullname</label>' +
+//   "</div>";
+// $(document).ready(function () {
+//   $("#login").addClass("active");
+//   $("#form").html(loginHTMLform);
+// });
+// $("#login").on("click", function () {
+//   if ($("#signup").hasClass("active")) {
+//     $("#signup").removeClass("active");
+//     $("#login").addClass("active");
+//     $("#form").html(loginHTMLform);
+//   }
+// });
+// $("#signup").on("click", function () {
+//   if ($("#login").hasClass("active")) {
+//     $("#login").removeClass("active");
+//     $("#signup").addClass("active");
+//     $("#form").html(signupHTMLform);
+//   }
+// });
