a495c275bcf3656acc4f71a70efdc1d359b87b55
Jon Ahmeti <joahmeti-asseco@outlook.com>
2021-11-18 16:31:24 +0100

    working on Employee Index UI/UX

---
+++ wwwroot/js/Areas/Employee/Home/index.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    let rightArrow = $("#infoCardsArrowRight");
+    let leftArrow = $("#infoCardsArrowLeft");
+    let infoCards = $("#infoCards");
+    let stopFlag = false;
+
+    //Crashes --- Fix it 
+    $(rightArrow).on("mouseenter", function () {
+        do {
+            let currentScroll = $(infoCards).scrollLeft() + 10;
+            $(infoCards).scrollLeft(currentScroll);
+        } while (!stopFlag);
+    });
+
+    $(rightArrow).on("mouseleave", function () {
+        stopFlag = true;
+    });
+});
