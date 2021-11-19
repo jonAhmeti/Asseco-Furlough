e6b39614ae82fd7a1aada27a9d2547a5dc67b489
Jon Ahmeti <joahmeti-asseco@outlook.com>
2021-11-19 09:07:26 +0100

    Header cards on Employee/Home/index done. Added animation using jquery

--- wwwroot/js/Areas/Employee/Home/index.js
+++ wwwroot/js/Areas/Employee/Home/index.js
@@ -2,17 +2,36 @@
     let rightArrow = $("#infoCardsArrowRight");
     let leftArrow = $("#infoCardsArrowLeft");
     let infoCards = $("#infoCards");
-    let stopFlag = false;
 
-    //Crashes --- Fix it 
-    $(rightArrow).on("mouseenter", function () {
-        do {
-            let currentScroll = $(infoCards).scrollLeft() + 10;
-            $(infoCards).scrollLeft(currentScroll);
-        } while (!stopFlag);
+    $(infoCards).on('mouseenter', function () {
+
+        $(rightArrow).animate({ 'right': 0 }, 300);
+        $(leftArrow).animate({ 'left': 0 }, 300);
     });
 
-    $(rightArrow).on("mouseleave", function () {
-        stopFlag = true;
+
+    $(infoCards).on('mouseleave', function () {
+        //if hovering over the left and right arrows
+        if ($("#infoCardsArrowRight:hover").length != 0 || $("#infoCardsArrowLeft:hover").length != 0)
+            return;
+
+        $(rightArrow).animate({ 'right': '-100px' }, 300);
+        $(leftArrow).animate({ 'left': '-100px' }, 300);
     });
+
+    $(rightArrow).on("click", function () {
+        let item_width = $('.card').outerWidth(true);
+        console.log(item_width);
+        let scrollVal = $(infoCards).scrollLeft();
+        $(infoCards).animate({ scrollLeft: scrollVal + item_width }, 500);
+    });
+
+
+    $(leftArrow).on("click", function () {
+        let item_width = $('.card').outerWidth(true);
+        console.log(item_width);
+        let scrollVal = $(infoCards).scrollLeft();
+        $(infoCards).animate({ scrollLeft: scrollVal - item_width }, 500);
+    });
+
 });
