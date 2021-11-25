04f6590bc14180545dc17233d82e0046aa435ac8
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2021-11-25 08:34:18 +0100

    Shared resource created and working

--- Resources/Shared/_Admin_Layout_en.Designer.cs
+++ Resources/Shared/_Admin_Layout_en.Designer.cs
@@ -22,14 +22,14 @@ namespace Resources.Shared {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class _adminLayout {
+    public class _Admin_Layout_en {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal _adminLayout() {
+        internal _Admin_Layout_en() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Resources.Shared {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Furlough.Resources.Shared._adminLayout", typeof(_adminLayout).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Furlough.Resources.Shared._Admin_Layout_en", typeof(_Admin_Layout_en).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -93,6 +93,15 @@ namespace Resources.Shared {
         public static string edit {
             get {
                 return ResourceManager.GetString("edit", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Save.
+        /// </summary>
+        public static string save {
+            get {
+                return ResourceManager.GetString("save", resourceCulture);
             }
         }
     }
