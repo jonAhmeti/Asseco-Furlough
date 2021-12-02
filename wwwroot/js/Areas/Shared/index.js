0667440939d20e4f01d61c788c4178d363105de7
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2021-12-02 12:36:52 +0100

    Responsive index.cshtml tables and header up to 256px mobile width

--- wwwroot/js/Areas/Shared/index.js
+++ wwwroot/js/Areas/Shared/index.js
@@ -10,4 +10,22 @@ $(function () {
         }
     );
   }
+  $(window).on("load resize", function(){
+    if ($(window).width() < 470) {
+      $(".tableDiv").removeClass("d-flex justify-content-center");
+      $(".tableDiv").addClass("overflow-auto");
+    }
+    if ($(window).width() > 470) {
+      $(".tableDiv").removeClass("overflow-auto");
+      $(".tableDiv").addClass("d-flex justify-content-center");
+    }
+    if ($(window).width() < 350) {
+      $(".roleTableDiv").removeClass("d-flex justify-content-center");
+      $(".roleTableDiv").addClass("overflow-auto");
+    }
+    if ($(window).width() > 350) {
+      $(".roleTableDiv").removeClass("overflow-auto");
+      $(".roleTableDiv").addClass("d-flex justify-content-center");
+    }
+  });
 });
