a8c30fffa7f5366b07795b6ca1db91f4ddeda365
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2021-12-02 09:45:49 +0100

    Hamburger funcionality working perfect

--- wwwroot/js/Areas/Layout/side.js
+++ wwwroot/js/Areas/Layout/side.js
@@ -2,8 +2,21 @@ $(function () {
   var sideNav = $("#sidenav");
   var sideHam = $("#sideHam");
   var menuCheckbox = $("#menu-btn");
+  $(window).resize(function(){
+    if ($(window).width() > 768) {
+      sideNav.removeAttr("style");
+      sideNav.css({"display": "none"});
+      sideNav.next().css({"display": "block"});
+      $(menuCheckbox).prop('checked', false);
+    }
+    if (($(window).width() < 768) && (menuCheckbox.is(":checked") == true)) {
+      sideNav.css({"display": "block", "padding-top": "5rem", "width": "100vw", "position": "absolute", "z-index": "99", "text-align": "center"});
+      sideNav.next().css({"display": "none"});
+    }
+  });
   $(sideHam).on("click", function () {
-    if ($(menuCheckbox).is(":checked")) {
+    if (menuCheckbox.is(":checked")) {
+      sideNav.removeAttr("style");
       sideNav.css({"display": "none"});
       sideNav.next().css({"display": "block"});
     } else {
