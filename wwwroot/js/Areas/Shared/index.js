76c46e6eb137fdcca137587d0a006a5f19628fc2
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2021-12-03 16:43:52 +0100

    Responsive Admin - Edit/Delete/Details/Create cshtml files

--- wwwroot/js/Areas/Shared/index.js
+++ wwwroot/js/Areas/Shared/index.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function () { 
   var rowIdArray = $("[rowId]");
   for (let i = 0; i < rowIdArray.length; i++) {
     $(`tr[rowId=${i}]`).find("a.rowLink").hover(
@@ -10,22 +10,31 @@ $(function () {
         }
     );
   }
+  var tableDiv = $(".tableDiv");
+  var roleTableDiv = $(".roleTableDiv");
+  var detailsWrapper = $("#detailsWrapper");
   $(window).on("load resize", function(){
     if ($(window).width() < 470) {
-      $(".tableDiv").removeClass("d-flex justify-content-center");
-      $(".tableDiv").addClass("overflow-auto");
+      $(tableDiv).removeClass("d-flex justify-content-center");
+      $(tableDiv).addClass("overflow-auto");
     }
     if ($(window).width() > 470) {
-      $(".tableDiv").removeClass("overflow-auto");
-      $(".tableDiv").addClass("d-flex justify-content-center");
+      $(tableDiv).removeClass("overflow-auto");
+      $(tableDiv).addClass("d-flex justify-content-center");
     }
-    if ($(window).width() < 350) {
-      $(".roleTableDiv").removeClass("d-flex justify-content-center");
-      $(".roleTableDiv").addClass("overflow-auto");
+    if ($(window).width() < 480) {
+      $(roleTableDiv).removeClass("d-flex justify-content-center");
+      $(roleTableDiv).addClass("overflow-auto");
     }
-    if ($(window).width() > 350) {
-      $(".roleTableDiv").removeClass("overflow-auto");
-      $(".roleTableDiv").addClass("d-flex justify-content-center");
+    if ($(window).width() > 480) {
+      $(roleTableDiv).removeClass("overflow-auto");
+      $(roleTableDiv).addClass("d-flex justify-content-center");
+    }
+    if ($(window).width() < 768) {
+      $(detailsWrapper).removeClass("ms-4");
+    }
+    if ($(window).width() > 768) {
+      $(detailsWrapper).addClass("ms-4");
     }
   });
 });
