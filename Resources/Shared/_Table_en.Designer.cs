b23a7f25cbb13ca67053fe60447cbce99ec930a8
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2021-12-06 11:39:41 +0100

    Resource Files

--- Resources/Shared/_Table_en.Designer.cs
+++ Resources/Shared/_Table_en.Designer.cs
@@ -22,14 +22,14 @@ namespace Resources.Shared {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class _Admin_Layout_en {
+    public class _Table_en {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal _Admin_Layout_en() {
+        internal _Table_en() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Resources.Shared {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Furlough.Resources.Shared._Admin_Layout_en", typeof(_Admin_Layout_en).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Furlough.Resources.Shared._Table_en", typeof(_Table_en).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -61,7 +61,7 @@ namespace Resources.Shared {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Back To List.
+        ///   Looks up a localized string similar to Back to list.
         /// </summary>
         public static string backToList {
             get {
