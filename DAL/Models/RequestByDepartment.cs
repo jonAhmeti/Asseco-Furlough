ce9ab8cdff85db3c3d67e64a9877722c3b3e7b8c
Jon Ahmeti <joahmeti-asseco@outlook.com>
2021-12-06 15:23:40 +0100

    New model dropped: RequestByDepartment

---
+++ DAL/Models/RequestByDepartment.cs
@@ -0,0 +1,15 @@
+﻿namespace Furlough.DAL.Models
+{
+    public class RequestByDepartment
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+        public string EmployeeName { get; set; }
+        public int EmployeeId { get; set; }
+        public int RequestId { get; set; }
+        public int RequestedByUserId { get; set; }
+        public DateOnly RequestDateFrom { get; set; }
+        public DateOnly RequestDateUntil { get; set; }
+        public DateTime RequestedOn { get; set; }
+    }
+}
