c9cbf0ff905bf96212a2f63ba4f944fd2560b48a
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2021-12-29 11:30:46 +0100

    Unused Functions Deleted

--- Areas/Manager/Controllers/RequestController.cs
+++ Areas/Manager/Controllers/RequestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -134,24 +135,5 @@ namespace Furlough.Areas.Manager.Controllers
 
             return View(request);
         }
-
-        [AllowAnonymous]
-        [HttpPost]
-
-        public IActionResult ChangeLang(string lang, string returnUrl)
-        {
-            var cultureInfo = new CultureInfo(lang);
-            Thread.CurrentThread.CurrentUICulture = cultureInfo;
-            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureInfo.Name);
-
-            this.HttpContext.Response.Cookies.Append
-            (
-                CookieRequestCultureProvider.DefaultCookieName,
-                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(lang))
-            );
-
-            return LocalRedirect(returnUrl);
-        }
-
     }
 }
