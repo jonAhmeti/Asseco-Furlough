4912f58e375a31f1c7c1de4110a1917863884955
Jon Ahmeti <joahmeti-asseco@outlook.com>
2021-12-30 10:07:08 +0100

    Responsive datatables in admin request/index

---
+++ wwwroot/js/Areas/Admin/Request/dataTables.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    //init of datatable
+    var table = $(".table").DataTable({
+        dom: "<'pt-5 px-3'<'d-flex justify-content-between'pf><t><'d-flex flex-column 'B>>",
+        buttons: true,
+        columnDefs: [
+            { targets: [4], sortable: false, searchable: false },
+        ]
+    });
+});
