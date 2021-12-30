a43a288f7ecf7a7290316fced397fd6a6823126a
Jon Ahmeti <joahmeti-asseco@outlook.com>
2021-12-30 09:35:14 +0100

    datatables design and seperate css/js in admin/Shared

---
+++ wwwroot/js/Areas/Admin/Shared/dataTables.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    //init of datatable
+    var table = $(".table").DataTable({
+        dom: "<'employeeRequestTableDiv mt-1 shadow rounded border'<'d-flex justify-content-between'pf><t><'d-flex flex-column 'B>>",
+        buttons: true,
+        columnDefs: [
+            { targets: [4], sortable: false },
+        ]
+    });
+});
