a864329c4f377c1203357b13573e840b829969d3
Jon Ahmeti <joahmeti-asseco@outlook.com>
2022-01-06 15:16:44 +0100

    printing table in availableDays Admin now contains asseco logo image added jszip neccessary for excel export and Html5 export types

--- wwwroot/js/Areas/Admin/AvailableDays/dataTables.js
+++ wwwroot/js/Areas/Admin/AvailableDays/dataTables.js
@@ -2,9 +2,33 @@
     //init of datatable
     var table = $(".table").DataTable({
         dom: "<'pt-5'<'d-flex justify-content-between px-3'pf><t><'d-flex flex-column 'B>>",
-        buttons: true,
-        //columnDefs: [
-        //    { targets: [13], sortable: false, searchable: false },
-        //]
+        buttons: [
+            //{
+            //    extend: 'copyHtml5',
+            //    title: 'Admin_AvailableDays-' + new Date().toISOString(),
+            //}, //doesn't copy correctly
+            {
+                extend: 'pdfHtml5',
+                orientation: 'landscape',
+                title: 'Admin_AvailableDays-' + new Date().toISOString()
+            },
+            {
+                extend: 'csvHtml5',
+                title: 'Admin_AvailableDays-' + new Date().toISOString(),
+            },
+            {
+                extend: 'print',
+                title: 'Admin_AvailableDays-' + new Date().toISOString(),
+                orientation: 'landscape',
+                exportOptions: {
+                    stripHtml: false
+                },
+                customize: function (win) {
+                    $(win.document.body)
+                        .prepend(
+                            `<img src="https://${window.location.host}/images/asseco_southeasterneurope_logo.svg" style="position:absolute; bottom:0; left:0; opacity:0.1; width:50%;"/>`
+                    ); //window.location.host gets localhost:port at the moment
+                }
+            }],
     });
 });
--- wwwroot/js/Areas/Admin/Request/dataTables.js
+++ wwwroot/js/Areas/Admin/Request/dataTables.js
@@ -2,7 +2,29 @@
     //init of datatable
     var table = $(".table").DataTable({
         dom: "<'pt-5'<'d-flex justify-content-between px-3'pf><t><'d-flex flex-column 'B>>",
-        buttons: true,
+        buttons: [
+            {
+                extend: 'pdf',
+                title: 'Admin_Request-' + new Date().toISOString(),
+                exportOptions: {
+                    columns: 'th:not(:last-child)'
+                }
+            },
+            {
+                extend: 'csv',
+                title: 'Admin_Request-' + new Date().toISOString(),
+                exportOptions: {
+                    columns: 'th:not(:last-child)'
+                }
+            },
+            {
+                extend: 'print',
+                title: 'Admin_Request-' + new Date().toISOString(),
+                exportOptions: {
+                    columns: 'th:not(:last-child)'
+                }
+                
+            }],
         columnDefs: [
             { targets: [4], sortable: false, searchable: false },
         ]
