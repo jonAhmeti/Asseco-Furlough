5e110fa3fed35140f0db8bb3013cc42662e94362
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2022-01-10 16:04:56 +0100

    Started making tabBar on Admin Dashboard function to switch between departments for different Employee

--- wwwroot/js/Areas/Admin/Home/index.js
+++ wwwroot/js/Areas/Admin/Home/index.js
@@ -1,1 +1,13 @@
-﻿
+﻿$(function () {
+    const departments = $(".tabBarItem");
+    
+    for (let i = 0; i < departments.length; i++) 
+    {   
+        $(departments[i]).on("click", function() 
+        {
+            if (!$(this).hasClass("activeBar")) {
+                console.log($(this).find("a").text());
+            }
+        });
+    }
+});
