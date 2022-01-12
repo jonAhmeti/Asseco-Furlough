45250f7765ecfde0bcefb3c14414e89e989165f4
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2022-01-12 13:03:24 +0100

    some js added

--- wwwroot/js/Areas/Admin/Home/index.js
+++ wwwroot/js/Areas/Admin/Home/index.js
@@ -1,12 +1,31 @@
 ﻿$(function () {
     const departments = $(".tabBarItem");
-    
-    for (let i = 0; i < departments.length; i++) 
-    {   
-        $(departments[i]).on("click", function() 
-        {
+    var employeeDep = $(".listItem");
+    const lookup = {
+        Banking: 1,
+        Test: 2,
+        AIS: 5,
+        CASD: 6,
+    };
+
+    for (let i = 0; i < departments.length; i++) {
+        $(departments[i]).on("click", function () {
             if (!$(this).hasClass("activeBar")) {
-                console.log($(this).find("a").text());
+                var tabText = $(this).find("a").text();
+                // employeeDep.forEach(element => {
+                //     var listDepText = element.text();
+                //     if (lookup[tabText] == listDepText) {
+                //         employeeDep[i].parent().addClass("d-block");
+                //     } else {
+                //         employeeDep[i].parent().addClass("d-none");
+                //     }
+                // });
+                for (let i = 0; i < employeeDep.length; i++) {
+                    var ar = employeeDep[i].getElementsByTagName("li");
+                    if ($("ul li").hasClass("col")) {
+                        console.log(ar[2]);
+                    }
+                }
             }
         });
     }
