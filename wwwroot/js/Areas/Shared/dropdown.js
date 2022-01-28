e1fd0c0e401b98810213301dcc93b05a26c999d6
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2022-01-28 14:37:44 +0100

    Sidenav is almost finished just need to make it responsive if necessary. Also created Canceled razor view in Request

--- wwwroot/js/Areas/Shared/dropdown.js
+++ wwwroot/js/Areas/Shared/dropdown.js
@@ -1,11 +1,36 @@
 $(function () {
   var reqDd = $("#requestDropdown");
   var reqDdToggle = $("#requestDropdownToggle");
+  var staffDd = $("#staffDropdown");
+  var staffDdToggle = $("#staffDropdownToggle");
+  var compDd = $("#compDropdown");
+  var compDdToggle = $("#compDropdownToggle");
+
   reqDdToggle.on("click", function () {
     if (!reqDd.hasClass("activeDropdown")) {
-      reqDd.animate({"height": "200px"},{duration:300,queue:false}).addClass("activeDropdown");
+      staffDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
+      compDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
+      reqDd.animate({"height": "270px"},{duration:300,queue:false}).addClass("activeDropdown");
     } else {
       reqDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
+    }
+  });
+  staffDdToggle.on("click", function () {
+    if (!staffDd.hasClass("activeDropdown")) {
+      compDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
+      reqDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
+      staffDd.animate({"height": "200px"},{duration:300,queue:false}).addClass("activeDropdown");
+    } else {
+      staffDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
+    }
+  });
+  compDdToggle.on("click", function () {
+    if (!compDd.hasClass("activeDropdown")) {
+      reqDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
+      staffDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
+      compDd.animate({"height": "135px"},{duration:300,queue:false}).addClass("activeDropdown");
+    } else {
+      compDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
     }
   });
 });
