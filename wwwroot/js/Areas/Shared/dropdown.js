6ae4da0004a7e22c6cffaa9752782c279847fd20
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2022-01-31 14:34:43 +0100

    Removed unnecessary code

--- wwwroot/js/Areas/Shared/dropdown.js
+++ wwwroot/js/Areas/Shared/dropdown.js
@@ -1,44 +1,6 @@
-// $(function () {
-//   var reqDd = $("#requestDropdown");
-//   var reqDdToggle = $("#requestDropdownToggle");
-//   var staffDd = $("#staffDropdown");
-//   var staffDdToggle = $("#staffDropdownToggle");
-//   var compDd = $("#compDropdown");
-//   var compDdToggle = $("#compDropdownToggle");
-
-//   reqDdToggle.on("click", function () {
-//     if (!reqDd.hasClass("activeDropdown")) {
-//       staffDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
-//       compDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
-//       reqDd.animate({"height": "270px"},{duration:300,queue:false}).addClass("activeDropdown");
-//     } else {
-//       reqDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
-//     }
-//   });
-//   staffDdToggle.on("click", function () {
-//     if (!staffDd.hasClass("activeDropdown")) {
-//       compDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
-//       reqDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
-//       staffDd.animate({"height": "200px"},{duration:300,queue:false}).addClass("activeDropdown");
-//     } else {
-//       staffDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
-//     }
-//   });
-//   compDdToggle.on("click", function () {
-//     if (!compDd.hasClass("activeDropdown")) {
-//       reqDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
-//       staffDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
-//       compDd.animate({"height": "135px"},{duration:300,queue:false}).addClass("activeDropdown");
-//     } else {
-//       compDd.animate({"height": "0px"},{duration:300,queue:false}).removeClass("activeDropdown");
-//     }
-//   });
-// });
 $(function () {
   var dropDownToggle = $(".dropdownToggle");
   dropDownToggle.next().slideUp(0);
-
-
 
   dropDownToggle.on("click", function () {
     var layoutDropdown = $(this).next();
