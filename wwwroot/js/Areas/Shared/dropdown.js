bef9d17bf98360dcfe893bed30091490e99b9139
Labeat Zekaj <94106567+Labeat@users.noreply.github.com>
2022-02-01 11:50:01 +0100

    Logout created as a partial view and Dropdown Glitch is fixed

--- wwwroot/js/Areas/Shared/dropdown.js
+++ wwwroot/js/Areas/Shared/dropdown.js
@@ -1,10 +1,14 @@
 $(function () {
   var dropDownToggle = $(".dropdownToggle");
   dropDownToggle.next().slideUp(0);
-
+  
+  setTimeout(function() {
+    dropDownToggle.next().removeClass("hideDropdown");
+  }, 1);
+  
   dropDownToggle.on("click", function () {
-    var layoutDropdown = $(this).next();
     
+    var layoutDropdown = $(this).next();
     if (!layoutDropdown.hasClass("activeDropdown")) {
       dropDownToggle.next().slideUp(500).removeClass("activeDropdown");
       layoutDropdown.slideDown(500).addClass("activeDropdown");
